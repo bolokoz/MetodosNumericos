89196df77221ba5edd874145085dd4dd14a9c1c5
Yuri Furusho Becker <yurifbeck@gmail.com>
2014-03-03 14:00:57 -0400

    Implementado lsita de metodos (main screen)

--- src/com/yurifbeck/metodosnumericos/MainActivity.java
+++ src/com/yurifbeck/metodosnumericos/MainActivity.java
@@ -1,18 +1,25 @@
 package com.yurifbeck.metodosnumericos;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
 	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
         
+        String[] metodos = new String[] { "bisseccao" };
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,R.id.listViewMain, metodos);
+            setListAdapter(adapter);
+  
         
     }
 
