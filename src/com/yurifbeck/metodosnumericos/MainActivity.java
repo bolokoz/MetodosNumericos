91cb7565eb3352ed7f36b566d489888fbd9d4cd8
Yuri Furusho Becker <yurifbeck@gmail.com>
2014-03-04 11:35:38 -0400

    Implementado lista de metodos

--- src/com/yurifbeck/metodosnumericos/MainActivity.java
+++ src/com/yurifbeck/metodosnumericos/MainActivity.java
@@ -1,14 +1,18 @@
 package com.yurifbeck.metodosnumericos;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 	
 	ListView list;
+	String[] metodos;
 
 	
 	
@@ -16,15 +20,37 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        String[] metodos = new String[] { "Bissecção" };
+         metodos = new String[] { "Bissecção" };
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, metodos);
             setListAdapter(adapter);
-        
     }
+    
+    
 
 
     @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		
+		switch (position){
+		
+		case 0:
+			Intent intent = new Intent(this, Bisseccao.class);
+			Toast toast = Toast.makeText(getApplicationContext(), "Metodo escolhido: " + metodos[position], Toast.LENGTH_LONG);
+			toast.show();
+			startActivity(intent);
+			break;
+		
+		}
+		
+	}
+
+
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
