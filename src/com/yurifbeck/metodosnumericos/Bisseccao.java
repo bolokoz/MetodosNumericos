29099c5d68504e47bafd1a36e916bcd5b46ff4c7
Yuri Furusho Becker <yurifbeck@gmail.com>
2014-03-06 02:02:33 -0400

    Metodo totalmente funcional

--- src/com/yurifbeck/metodosnumericos/Bisseccao.java
+++ src/com/yurifbeck/metodosnumericos/Bisseccao.java
@@ -18,9 +18,11 @@ import android.widget.Toast;
 
 public class Bisseccao extends ListActivity {
 
-	EditText primeiroTermo, intervaloMenor, intervaloMaior, erro, iteracoes;
+	EditText primeiroTermo, primeiroTermoElevado, segundoTermo,
+			segundoTermoElevado, termoIndependente, intervaloMenor,
+			intervaloMaior, erro, iteracoes;
 	Button calcular, reset;
-	TextView invisivelTexto;
+	TextView invisivelTexto, funcaoResultante;
 	ListView list;
 	TableRow invisivelLinha;
 	double a, b, c, error, x, raiz, fa, fb, fc;
@@ -39,6 +41,10 @@ public class Bisseccao extends ListActivity {
 		setContentView(R.layout.metodobisseccao);
 
 		primeiroTermo = (EditText) findViewById(R.id.primeiroTermoEditText);
+		primeiroTermoElevado = (EditText) findViewById(R.id.primeiroTermoElevadoEditText);
+		segundoTermo = (EditText) findViewById(R.id.SegundoTermoEditText);
+		segundoTermoElevado = (EditText) findViewById(R.id.segundoTermoElevadoEditText);
+		termoIndependente = (EditText) findViewById(R.id.termoIndependenteEditText);
 		intervaloMaior = (EditText) findViewById(R.id.intervaloMaiorEditText);
 		intervaloMenor = (EditText) findViewById(R.id.intervaloMenorEditText);
 		erro = (EditText) findViewById(R.id.erroEditText);
@@ -51,13 +57,118 @@ public class Bisseccao extends ListActivity {
 
 	public void calcular(View v) {
 
-		/*
-		 * x = double.parsedouble(primeiroTermo.getText().toString()); a =
-		 * double.parsedouble(intervaloMenor.getText().toString()); b =
-		 * double.parsedouble(intervaloMaior.getText().toString()); error =
-		 * double.parsedouble(erro.getText().toString()); i =
-		 * Integer.parseInt(iteracoes.getText().toString());
-		 */
+		if (!isEmpty()) {
+
+			double termo1 = Double
+					.valueOf((primeiroTermo.getText().toString()));
+			double termo1e = Double.valueOf(primeiroTermoElevado.getText()
+					.toString());
+			double termo2 = Double.valueOf(segundoTermo.getText()
+					.toString());
+			double termo2e = Double.valueOf(segundoTermoElevado.getText()
+					.toString());
+			double termoInddy = Double.valueOf(termoIndependente.getText()
+					.toString());
+
+			double a = Double.valueOf((intervaloMenor.getText().toString()));
+			double b = Double.valueOf(intervaloMaior.getText().toString());
+			double error = Double.parseDouble(erro.getText().toString());
+			int i = Integer.parseInt((iteracoes.getText().toString()));
+
+			a_array = new double[i];
+			b_array = new double[i];
+			c_array = new double[i];
+			fc_array = new double[i];
+
+			fa = Math.pow((termo1 * a), termo1e)
+					+ Math.pow((termo2 * a), termo2e) + termoInddy;
+			fb = Math.pow((termo1 * b), termo1e)
+					+ Math.pow((termo2 * b), termo2e) + termoInddy;
+			j = i; // aux
+			i = 0; // aux
+
+			do {
+				// Primeiro passo, achar f(a) e f(b) e garantir que f(a).f(b) <
+				// 0
+
+				if (fa == 0) {
+					raiz = a;
+					ocorrencia = "Intervalo menor é raiz";
+
+					break;
+				} else if (fb == 0) {
+					raiz = b;
+					ocorrencia = "Intervalo maior é raiz";
+
+					break;
+				} else if (fa*fb> 0) {
+					ocorrencia = "Não há raiz ou há mais de uma no intervalo";
+
+					break;
+				} else {
+
+					c = (a + b) / 2;
+					fc = Math.pow((termo1 * c), termo1e)
+							+ Math.pow((termo2 * c), termo2e) + termoInddy;
+
+					a_array[i] = a;
+					b_array[i] = b;
+					c_array[i] = c;
+					fc_array[i] = fc;
+
+					if (fc * fa < 0) {
+
+						b = c;
+						i++;
+
+					} else {
+
+						a = c;
+						i++;
+					}
+
+					fc = (fc <= 0.0F) ? 0.0F - fc : fc; // valor absoluto de
+														// funcao
+														// de c
+
+					if (fc < error) {
+						margem = false;
+					}
+
+				}
+			} while ((i != j) && margem);
+
+			if (ocorrencia.isEmpty()) {
+
+				margem = true;
+				invisivelLinha.setVisibility(View.VISIBLE);
+
+				setListAdapter(new BisseccaoAdapter(a_array, b_array, c_array,
+						fc_array));
+
+			} else {
+				invisivelLinha.setVisibility(View.GONE);
+				invisivelTexto.setVisibility(View.VISIBLE);
+				invisivelTexto.setText(ocorrencia);
+				Toast toast = Toast.makeText(getApplicationContext(),
+						ocorrencia, Toast.LENGTH_LONG);
+				toast.show();
+			}
+		}else{
+			ocorrencia = "Cheque os campos";
+			Toast toast = Toast.makeText(getApplicationContext(),
+					ocorrencia, Toast.LENGTH_LONG);
+			toast.show();
+		}
+	}
+
+	public void resetar(View v) {
+
+		primeiroTermo.setText("");
+		intervaloMenor.setText("");
+		intervaloMaior.setText("");
+		erro.setText("");
+		iteracoes.setText("");
 
 		x = 1f;
 		a = -2.0f;
@@ -124,33 +235,37 @@ public class Bisseccao extends ListActivity {
 		} while ((i != j) && margem);
 
 		if (ocorrencia.isEmpty()) {
-			
-			invisivelLinha.setVisibility(View.VISIBLE);
 
-			Toast toast = Toast.makeText(getApplicationContext(),
-					Double.toString(fc_array[2]), Toast.LENGTH_LONG);
-			toast.show();
+			margem = true;
+			invisivelLinha.setVisibility(View.VISIBLE);
 
 			setListAdapter(new BisseccaoAdapter(a_array, b_array, c_array,
 					fc_array));
 
 		} else {
+			invisivelLinha.setVisibility(View.GONE);
 			invisivelTexto.setVisibility(View.VISIBLE);
 			invisivelTexto.setText(ocorrencia);
 			Toast toast = Toast.makeText(getApplicationContext(), ocorrencia,
 					Toast.LENGTH_LONG);
 			toast.show();
 		}
+
 	}
 
-	public void resetar(View v) {
+	public boolean isEmpty() {
 
-		primeiroTermo.setText("");
-		intervaloMenor.setText("");
-		intervaloMaior.setText("");
-		erro.setText("");
-		iteracoes.setText("");
+		if (primeiroTermo.getText().toString().isEmpty()
+				|| primeiroTermoElevado.getText().toString().isEmpty()
+				|| segundoTermo.getText().toString().isEmpty()
+				|| segundoTermoElevado.getText().toString().isEmpty()
+				|| intervaloMaior.getText().toString().isEmpty()
+				|| intervaloMenor.getText().toString().isEmpty()
+				|| iteracoes.getText().toString().isEmpty()) {
 
+			return true;
+		} else
+			return false;
 	}
 
 	@Override
@@ -205,14 +320,14 @@ public class Bisseccao extends ListActivity {
 
 		@Override
 		public int getCount() {
-			
-			for(i=0;i<a2.length;i++){
-				if(a2[i]==0){
+
+			for (i = 0; i < a2.length; i++) {
+				if (a2[i] == 0) {
 					break;
 				}
-					
+
 			}
-			
+
 			return i;
 		}
 
